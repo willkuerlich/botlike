18810b3b5d982ec70f54a82d1dcbe6010df289ec
Willkuerlich <8741947+willkuerlich@users.noreply.github.com>
2023-03-24 01:07:35 +0100

    remove old bot config

--- src/app/baseBotConfig.ts
+++
@@ -1,8 +0,0 @@
-// import { service } from 'src/botlike/bot/default/state/PromptMachine';
-import { BotlikeModuleConfig } from 'src/botlike/core/botlike.types';
-
-export const baseBotConfig: BotlikeModuleConfig = {
-  name: 'DefaultBot',
-  triggerSequence: '!',
-  // service,
-};
